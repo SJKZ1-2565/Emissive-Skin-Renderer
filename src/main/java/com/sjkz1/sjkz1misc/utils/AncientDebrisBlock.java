b8bf1014aca6500408329501b079d737e34b854a
SJKZ1-2565 <seefuture13@gmail.com>
2021-09-21 16:59:09 +0700

    Delete unused class

--- src/main/java/com/sjkz1/sjkz1misc/utils/AncientDebrisBlock.java
+++
@@ -1,7 +0,0 @@
-package com.sjkz1.sjkz1misc.utils;
-
-public class AncientDebrisBlock extends AbstractAncientDebrisBlock {
-    public AncientDebrisBlock(Settings settings) {
-        super(settings);
-    }
-}
