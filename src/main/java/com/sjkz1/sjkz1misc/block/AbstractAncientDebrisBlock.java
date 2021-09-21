8132a9a768da1f20536ab451234df66aeeb018ca
SJKZ1-2565 <seefuture13@gmail.com>
2021-09-21 16:57:55 +0700

    Clean up

--- src/main/java/com/sjkz1/sjkz1misc/block/AbstractAncientDebrisBlock.java
+++ src/main/java/com/sjkz1/sjkz1misc/block/AbstractAncientDebrisBlock.java
@@ -1,51 +1,36 @@
 package com.sjkz1.sjkz1misc.block;
 
-import java.awt.*;
-import java.util.Random;
-
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.sjkz1.sjkz1misc.SJKZ1Misc;
-
-import com.sjkz1.sjkz1misc.utils.SJKZ1Helper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+
+import java.awt.*;
+import java.util.Random;
 
 public abstract class AbstractAncientDebrisBlock extends Block {
 
     public AbstractAncientDebrisBlock(Settings settings) {
         super(settings);
     }
+    public static final Identifier BEAM_TEXTURE = new Identifier("textures/entity/beacon_beam.png");
+
 
     @Override
 	public void randomDisplayTick(BlockState blockState, World world, BlockPos blockPos, Random random) {
+        MatrixStack matrixStack = new MatrixStack();
+        VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+        float ticks = ((MinecraftClient.getInstance().player != null ? MinecraftClient.getInstance().player.age : 0) % 20 + MinecraftClient.getInstance().getTickDelta()) / 20.0F;
+        Color color = Color.getHSBColor(ticks, 0.9f, 1);
         if (SJKZ1Misc.CONFIG.getConfig().ShowDebrisUnderFire) {
-
-            PlayerEntity playerEntity = world.getClosestPlayer((double)blockPos.getX() + 0.5D, (double)blockPos.getY() + 0.5D, (double)blockPos.getZ() + 0.5D, 3.0D, false);
-            if (playerEntity != null) {
-                float ticks = (playerEntity.age % 20 + MinecraftClient.getInstance().getTickDelta()) / 20.0F;
-                Color color = Color.getHSBColor(ticks,0.9f,1);
-
-                if(SJKZ1Misc.CONFIG.getConfig().showFps)
-                {
-                    RenderSystem.sca;
-                    matrixStack.scale(4F,4F,4F);
-                    matrixStack.translate((float)(width / 2), (float)(height / 2), 0.0F);
-                    TextRenderer textRenderer =  mc.textRenderer;
-                    textRenderer.draw(matrixStack,text,width,height,formatting.getColorIndex());
-                    matrixStack.pop();
-
-                }
-            }
-
 
             int x;
             double posX;
