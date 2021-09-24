ba9664617ffe905e5e90bdabaf55e9e759892006
SJKZ1-2565 <seefuture13@gmail.com>
2021-09-24 15:44:00 +0700

    Remove useless file

--- src/main/java/com/sjkz1/minetils/block/AncientDebrisBlock.java
+++
@@ -1,9 +0,0 @@
-package com.sjkz1.minetils.block;
-
-import net.minecraft.world.level.block.state.BlockBehaviour;
-
-public class AncientDebrisBlock extends AbstractAncientDebrisBlock {
-    public AncientDebrisBlock(BlockBehaviour.Properties settings) {
-        super(settings);
-    }
-}
