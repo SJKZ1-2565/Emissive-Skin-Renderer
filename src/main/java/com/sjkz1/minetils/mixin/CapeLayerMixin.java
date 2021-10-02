fd3a224aedb31a158bbae463260c7d4ae47739a0
SJKZ1-2565 <seefuture13@gmail.com>
2021-10-02 15:37:01 +0700

    temporaty added

---
+++ src/main/java/com/sjkz1/minetils/mixin/CapeLayerMixin.java
@@ -0,0 +1,30 @@
+package com.sjkz1.minetils.mixin;
+
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.CapeFeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CapeFeatureRenderer.class)
+public abstract class CapeLayerMixin extends FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+
+
+    public CapeLayerMixin(FeatureRendererContext<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> featureRendererContext) {
+        super(featureRendererContext);
+    }
+
+    @Inject(method = "render",at = @At(value = "INVOKE",target = "Lnet/minecraft/client/render/VertexConsumerProvider;getBuffer(Lnet/minecraft/client/render/RenderLayer;)Lnet/minecraft/client/render/VertexConsumer;"))
+   public void specialCape(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci)
+   {
+
+   }
+}
---
+++ src/main/java/com/sjkz1/minetils/screen/SpecialMemberScreen.java
@@ -0,0 +1,57 @@
+package com.sjkz1.minetils.screen;
+
+
+import com.google.common.base.Function;
+import com.sjkz1.minetils.mixin.PlayerEntityMixin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+public class SpecialMemberScreen extends Screen {
+
+    private int leftPos;
+    private int topPos;
+
+    public SpecialMemberScreen(Text text) {
+        super(text);
+    }
+
+
+    @Override
+    protected void init() {
+        super.init();
+    }
+
+    @Override
+    public void render(MatrixStack mat, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(mat);
+        drawCenteredText(mat, this.textRenderer,Text.of("Special Member Wardrobe"), this.width / 2, 15, Formatting.GOLD.getColorValue());
+        drawCenteredText(mat, this.textRenderer, Text.of(Formatting.BOLD + "Feature for this screen, Not Soon..."), this.width / 2 , 25, Formatting.RED.getColorValue());
+        super.render(mat, mouseX, mouseY, partialTicks);
+        int k = leftPos;
+        int l = topPos;
+        InventoryScreen.drawEntity(k + 51, l + 75, 30, (float)(k + 51) - mouseX, (float)(l + 75 - 50) - mouseY, this.client.player);
+
+        PlayerInventory inv = this.client.player.getInventory();
+        ItemStack itemStack = inv.getMainHandStack();
+            if(itemStack != null) {
+                for(int i = 0;i >=0;) {
+                    renderItem(itemStack, width / 2, 35);
+                }
+            }
+    }
+
+    public static void renderItem(ItemStack itemStack, int x, int y)
+    {
+        var itemRender = MinecraftClient.getInstance().getItemRenderer();
+        itemRender.zOffset = -200.0F;
+        itemRender.renderInGui(itemStack, x, y);
+        itemRender.zOffset = 0.0F;
+    }
+}
