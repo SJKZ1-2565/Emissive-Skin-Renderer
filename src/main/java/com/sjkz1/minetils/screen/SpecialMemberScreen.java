52dd6b5f6b4dfe2809cc03fed48da223ff7bb69c
SJKZ1-2565 <seefuture13@gmail.com>
2021-10-14 17:02:46 +0700

    Update special member screen

--- src/main/java/com/sjkz1/minetils/screen/SpecialMemberScreen.java
+++ src/main/java/com/sjkz1/minetils/screen/SpecialMemberScreen.java
@@ -2,14 +2,17 @@ package com.sjkz1.minetils.screen;
 
 
 import com.google.gson.Gson;
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.sjkz1.minetils.Minetils;
 import com.sjkz1.minetils.utils.Donate;
 import com.sjkz1.minetils.utils.SJKZ1Helper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
@@ -17,6 +20,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3f;
@@ -25,10 +29,19 @@ import java.awt.*;
 
 public class SpecialMemberScreen extends Screen {
 
+
+    public Identifier BG = new Identifier(Minetils.MOD_ID + ":textures/gui/background.png");
     private float playerXRot = 0;
     private float ticks = 0;
     private static final Gson GSON = new Gson();
     public static int DONATE_TOTAL;
+
+    protected int x;
+    protected int y;
+
+    protected int backgroundWidth = 256;
+    protected int backgroundHeight = 166;
+
 
     public SpecialMemberScreen(Text text) {
         super(text);
@@ -36,8 +49,7 @@ public class SpecialMemberScreen extends Screen {
 
 
     @Override
-    protected void init()
-    {
+    protected void init() {
         super.init();
         int j = this.height / 4 + 48;
 
@@ -50,25 +62,43 @@ public class SpecialMemberScreen extends Screen {
 
     @Override
     public void render(MatrixStack mat, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(mat);
         ticks += 0.005F * partialTicks;
-
         playerXRot -= 0.15 * partialTicks;
-        if(playerXRot <= -179.85) {
+        if (playerXRot <= -179.85) {
             playerXRot = 180;
         }
-        Color color = Color.getHSBColor(ticks,0.9f,1);
-        String name = Minetils.SPECIAL_MEMBER.toString();
-        String formattedText = name.replace("[","");
-        formattedText = formattedText.replace("]","");
-        formattedText = formattedText.replace(",","");
-        drawCenteredText(mat, this.textRenderer,Text.of("Special Member Wardrobe"), this.width / 2, 15, Formatting.GOLD.getColorValue());
-        drawCenteredText(mat, this.textRenderer,Text.of("Total Donate " + DONATE_TOTAL), this.width / 2, 25, Formatting.GOLD.getColorValue());
-        drawCenteredText(mat, this.textRenderer, Text.of(Formatting.BOLD + "Feature for this screen, 35%"), this.width / 2 , 35, Formatting.RED.getColorValue());
-        drawCenteredText(mat, this.textRenderer, Text.of(formattedText), this.width / 2 , 65, color.getRGB());
+
+        renderBackgroundGG(mat);
         super.render(mat, mouseX, mouseY, partialTicks);
-        renderEntityInInventory(this.width / 2 - 165,155,  65,playerXRot,0,this.client.player);
         //renderAxolotlEntityInInventory(this.width / 2 - 165, 40,  35,playerXRot,0,new AxolotlEntity(EntityType.AXOLOTL,client.world));
+    }
+
+    public void renderBackgroundGG(MatrixStack matrixStack) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, BG);
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA);
+        int k = this.x;
+        int l = this.y;
+        this.drawTexture(matrixStack, k/2+120 , 15, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        Color color = Color.getHSBColor(ticks, 0.9f, 1);
+        String name = Minetils.SPECIAL_MEMBER.toString();
+        String SpecialMemberName = name.replace("[", "");
+        SpecialMemberName = SpecialMemberName.replace("]", "");
+        SpecialMemberName = SpecialMemberName.replace(",", "");
+        if(SpecialMemberName != null)
+        {
+            matrixStack.push();
+            matrixStack.translate(k/2+100,15,100);
+            matrixStack.scale(0.75F,0.75F,0.75F);
+            drawStringWithShadow(matrixStack, this.textRenderer, Text.of("✔ Special Member Wardrobe").asString(), k / 2 + 135, 15, Formatting.DARK_GREEN.getColorValue());
+            drawCenteredText(matrixStack, this.textRenderer, Text.of("☀ Total Donate " + DONATE_TOTAL).asString(), k / 2+200, 25, Formatting.GOLD.getColorValue());
+            drawCenteredText(matrixStack, this.textRenderer, Text.of("‼ Feature for this screen, 37%").asString(), k / 2+200, 35, Formatting.RED.getColorValue());
+            drawCenteredText(matrixStack, this.textRenderer, Text.of(SpecialMemberName).asString(), k / 2  + 220, 65, color.getRGB());
+            matrixStack.pop();
+        }
+        renderEntityInInventory(k / 2 + 136,67,  25,playerXRot,0,this.client.player);
     }
 
     //Taken from https://github.com/Intro-Dev/Osmium/blob/fabric/1.17.x/src/main/java/com/intro/client/render/screen/OsmiumCapeOptionsScreen.java
@@ -83,7 +113,7 @@ public class SpecialMemberScreen extends Screen {
         RenderSystem.applyModelViewMatrix();
         MatrixStack poseStack2 = new MatrixStack();
         // translate far from screen
-        poseStack2.translate(0.0D, 0.0D, 1000.0D);
+        poseStack2.translate(0.0D, 0.0D, 1000);
         poseStack2.scale((float)scale, (float)scale, (float)scale);
         // flip to right side
         Quaternion zRotationQuaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180.0F);
