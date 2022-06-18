11695703c19a9c5d1310a77d6473e273b1ac73aa
SJKZ1-2565 <seefuture13@gmail.com>
2022-06-18 09:23:16 +0700

    Remove some chat line

--- src/main/java/com/sjkz1/minetils/utils/ClientInit.java
+++ src/main/java/com/sjkz1/minetils/utils/ClientInit.java
@@ -1,9 +1,7 @@
 package com.sjkz1.minetils.utils;
 
 import com.sjkz1.minetils.Minetils;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientPacketListener;
 
 public class ClientInit {
     public static void tick(Minecraft minecraft) {
@@ -15,11 +13,8 @@ public class ClientInit {
             var i = (int) minecraft.player.getX();
             var j = (int) minecraft.player.getY();
             var k = (int) minecraft.player.getZ();
-            String pos = "X:" + i + " Y:" + j + " Z:" + k;
             String NetherPos = "Nether position X:" + i / 8 + " Y:" + j + " Z:" + k / 8;
             String OverWorldPose = "OverWorld position X:" + i * 8 + " Y:" + j + " Z:" + k * 8;
-
-            SJKZ1Helper.sendChat(pos);
             if (minecraft.player.level.dimensionType().piglinSafe()) {
                 SJKZ1Helper.sendChat(OverWorldPose);
             } else {
