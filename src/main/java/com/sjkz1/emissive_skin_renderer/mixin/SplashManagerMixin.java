603ca5ebe1c5f4acf4bd37a97073f99da7d65a0e
SJKZ1-2565 <seefuture13@gmail.com>
2022-10-12 15:43:19 +0700

    Added Easter Egg

---
+++ src/main/java/com/sjkz1/emissive_skin_renderer/mixin/SplashManagerMixin.java
@@ -0,0 +1,26 @@
+package com.sjkz1.emissive_skin_renderer.mixin;
+
+import net.minecraft.client.resources.SplashManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Mixin(SplashManager.class)
+public abstract class SplashManagerMixin extends SimplePreparableReloadListener<List<String>> {
+
+    @Inject(method = "getSplash",at = @At("TAIL"))
+    public void  injectMyBirthDay(CallbackInfoReturnable<String> cir)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        if (calendar.get(2) + 1 == 6 && calendar.get(5) == 3) {
+            cir.setReturnValue("§dHappy birthday SJKZ1!");
+        }
+    }
+}
